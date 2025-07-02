e1ca8da41c5d58e31db252d501154d699dc3a63e
Marcos Motta <mrcsmotta1@gmail.com>
2025-07-02 19:36:10 -0300

    Listing all users and listing by id

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,10 +1,32 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
+	"github/mrcsmotta1/go-rest-api-alura/models"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Welcome to the Home Page!")
+}
+
+func GetPersonalidades(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(models.Personalidades)
+}
+
+func GetPersonalidadeByID(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	for _, personalidade := range models.Personalidades {
+		if fmt.Sprintf("%d", personalidade.ID) == id {
+			json.NewEncoder(w).Encode(personalidade)
+			return
+		}
+	}
+
+	http.Error(w, "Personalidade not found", http.StatusNotFound)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github/mrcsmotta1/go-rest-api-alura
 
 go 1.24.4
+
+require github.com/gorilla/mux v1.8.1 // indirect
