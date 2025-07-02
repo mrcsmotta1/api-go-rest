30adc537ce3bc0b1833aa17ba80e970c3e3ed82c
Marcos Motta <mrcsmotta1@gmail.com>
2025-07-02 19:05:04 -0300

    Modularizing controller and routes

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github/mrcsmotta1/go-rest-api-alura/routes"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Starting server on port 8080...")
+	routes.HandleRequests()
 }
